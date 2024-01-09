eb5d2b29ed00bf976d565fecaf3d5aa621762e3c
pranramesh <pranavramesh4@gmail.com>
2024-01-09 12:29:08 -0800

    start

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 from langchain_community.embeddings import CohereEmbeddings
 
-embeddings = CohereEmbeddings(model="embed-english-light-v3.0", cohere_api_key="5vr3ldpOnOHoept93vDSgBPLGfqbZgBfnNeoaATZ")
+embeddings = CohereEmbeddings(model="embed-english-light-v3.0", cohere_api_key="YOUR_API_KEY")
 text = "Test"
 query_result = embeddings.embed_query(text)
 print(query_result)
