41389541642a15a9e4401327861e65dbd25ac7bc
pranramesh <pranavramesh4@gmail.com>
2024-01-09 19:12:58 -0800

    need to add pca and t-sne visualizations

--- model.py
+++ model.py
@@ -1,6 +1,32 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
+# import torch
+# import torch.nn as nn
+# import torch.optim as optim
+import numpy as np
+import pandas as pd 
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+
+#### Alternative Idea ####
+# 1. Given some text from user, make a call to cohere language model api and ask it to make the statement more negative in connotation
+# 2. re-embed this text using cohere embed model
+# 3. visualize this more "negative" embedding in 3d space and compare it with original embedding (can try using PCA and t-SNE and see which gives better results)
+
+class Visualizations():
+
+    def __init__(self, original_embedding, modified_embedding):
+        self.original_embedding = original_embedding
+        self.modified_embedding = modified_embedding
+
+    def principal_component_analysis(self):
+        pca = PCA(n_components=2)
+        og = pca.fit_transform(np.array([self.original_embedding]))
+        mod = pca.fit_transform(np.array([self.new_embedding]))
+        return [og, mod]
+
+    # def stochastic_neighbords(self, )
+
+
 
 
 #### Flow of Data in Application ####
@@ -11,24 +37,22 @@ import torch.optim as optim
 # 5. User can choose how to toggle the embedding.
 # 6. The modified embedding is then passed through the decoder which gives back the modified text reconstructed purely from the latent space embedding
 
-class Autoencoder(nn.Module):
-    #use cosin smilarity for loss metric
-    def __init__(self, input_dim, embedding_dim):
-        super().__init__()
-        self.encoder = nn.Sequential(
-            nn.Linear(in_features=input_dim, out_features=embedding_dim)
-            nn.ReLU()
-            nn.Linear(in_features=embedding_dim, out_features=embedding_dim)
-            nn.ReLU()
-        )
+# class Autoencoder(nn.Module):
+#     #use cosin smilarity for loss metric
+#     def __init__(self, input_dim, embedding_dim):
+#         super().__init__()
+#         self.encoder = nn.Sequential(
+#             nn.Linear(in_features=input_dim, out_features=embedding_dim), 
+#             nn.ReLU(), 
+#             nn.Linear(in_features=embedding_dim, out_features=embedding_dim), 
+#         )
 
-        self.decoder = nn.Sequential(
-            nn.Linear(in_features=embedding_dim, out_features=embedding_dim)
-            nn.ReLU()
-            nn.Linear(in_features=embedding_dim, out_features=input_dim)
-            nn.ReLU()
-        )
-    def forward(self, x):
-        embedding = self.encoder(x)
-        reconstructed = self.decoder(embedding)
-        return reconstructed
+#         self.decoder = nn.Sequential(
+#             nn.Linear(in_features=embedding_dim, out_features=embedding_dim), 
+#             nn.ReLU(), 
+#             nn.Linear(in_features=embedding_dim, out_features=input_dim), 
+#         )
+#     def forward(self, x):
+#         embedding = self.encoder(x)
+#         reconstructed = self.decoder(embedding)
+#         return reconstructed
--- script.py
+++ script.py
@@ -2,30 +2,63 @@ from langchain_community.embeddings import CohereEmbeddings
 import streamlit as st
 import pandas as pd 
 import torch
-
-embeddings = CohereEmbeddings(model="embed-english-light-v3.0", cohere_api_key="5vr3ldpOnOHoept93vDSgBPLGfqbZgBfnNeoaATZ")
-
+import cohere
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+from model import Visualizations
+from matplotlib import pyplot as plt
 
 st.title("Latent Space Exploration")
 
+##### Container 1 #####
 container1 = st.container(border=True)
-container1.subheader("Original Text")
-text = container1.text_input('Text', label_visibility="hidden")
-container1.write(f"{text}")
+container1.header("Original Text")
+text = container1.text_area('Text', label_visibility="hidden")
+# container1.write(f"{text}")
+#allows user to choose whether to make text more negative or more positive
+option = container1.selectbox(
+    'How would you like to modify your text?',
+    ('very negative', 'negative', 'positive', 'very positive'), index=None, placeholder='Choose an option...')
 
-query_result = embeddings.embed_query(text)
 
-container2 = st.container(border=True)
-container2.subheader("Latent Space Embedding")
-container2.write(f"{query_result}")
+#initializing embedding model
+embeddings = CohereEmbeddings(model="embed-english-light-v3.0", cohere_api_key="5vr3ldpOnOHoept93vDSgBPLGfqbZgBfnNeoaATZ")
 
-container3 = st.container()
-number = container3.slider("Negativity", 0, 100)
+#initializing language model
+co = cohere.Client("5vr3ldpOnOHoept93vDSgBPLGfqbZgBfnNeoaATZ")
 
-container4 = st.container(border=True)
-container4.subheader("Reconstruction")
-container4.write(f"{[x + float(number) for x in query_result]}")
+if text and option:
 
-container5 = st.container(border=True)
-container5.subheader("Visualization")
-#project both embeddings into 2d subspace using PCA and display on app and show before and after
+    response = co.generate(
+    prompt=f"Please make the following block of text sound {option}: {text}"
+    )
+
+    original_embedding = embeddings.embed_query(text)
+    new_embedding = embeddings.embed_query(response[0])
+
+    ##### Container 2 #####
+    container2 = st.container(border=True)
+    container2.header("Modified Text")
+    container2.write(f"{response[0]}")
+
+    ##### Container 3 #####
+    container3 = st.container(border=True)
+    container3.header("Visualization")
+
+    #calculates cosin similarity
+    e1 = np.array(original_embedding)
+    e2 = np.array(new_embedding)
+    # Reshape the arrays to match the expected input shape of cosine_similarity
+    e1_embedding = e1.reshape(1, -1)
+    e2_embedding = e2.reshape(1, -1)
+    # Calculate cosine similarity
+    similarity = cosine_similarity(e1_embedding, e2_embedding)[0][0]
+    container3.write(f"Cosine similarity score: {similarity}")
+
+    #creates visualization for PCA
+    visualize = Visualizations(original_embedding, new_embedding)
+    principal_components = visualize.principal_component_analysis()
+
+    for i in principal_components:
+        fig = plt.scatter(i[:, 0], i[:, 1], label=(f'Original Embedding' if i == 0 else f'Modified Embedding'))
+        container3.pyplot(fig)
