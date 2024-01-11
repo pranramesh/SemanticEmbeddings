a0e7138ce3cae58cee886936fd74e2706025fbd4
pranramesh <pranavramesh4@gmail.com>
2024-01-10 16:08:12 -0800

    added temp autoencoder

--- model.py
+++ model.py
@@ -30,10 +30,6 @@ class Visualizations():
         tsne_data = tsne.fit_transform(m)
         return tsne_data
 
-
-
-
-
 #### Flow of Data in Application ####
 # 1. user gives text input
 # 2. Text input is then tokenized into vector
@@ -42,22 +38,22 @@ class Visualizations():
 # 5. User can choose how to toggle the embedding.
 # 6. The modified embedding is then passed through the decoder which gives back the modified text reconstructed purely from the latent space embedding
 
-# class Autoencoder(nn.Module):
-#     #use cosin smilarity for loss metric
-#     def __init__(self, input_dim, embedding_dim):
-#         super().__init__()
-#         self.encoder = nn.Sequential(
-#             nn.Linear(in_features=input_dim, out_features=embedding_dim), 
-#             nn.ReLU(), 
-#             nn.Linear(in_features=embedding_dim, out_features=embedding_dim), 
-#         )
+class Autoencoder(nn.Module):
+    #use cosin smilarity for loss metric
+    def __init__(self, input_dim, embedding_dim):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(in_features=input_dim, out_features=embedding_dim), 
+            nn.ReLU(), 
+            nn.Linear(in_features=embedding_dim, out_features=embedding_dim), 
+        )
 
-#         self.decoder = nn.Sequential(
-#             nn.Linear(in_features=embedding_dim, out_features=embedding_dim), 
-#             nn.ReLU(), 
-#             nn.Linear(in_features=embedding_dim, out_features=input_dim), 
-#         )
-#     def forward(self, x):
-#         embedding = self.encoder(x)
-#         reconstructed = self.decoder(embedding)
-#         return reconstructed
+        self.decoder = nn.Sequential(
+            nn.Linear(in_features=embedding_dim, out_features=embedding_dim), 
+            nn.ReLU(), 
+            nn.Linear(in_features=embedding_dim, out_features=input_dim), 
+        )
+    def forward(self, x):
+        embedding = self.encoder(x)
+        reconstructed = self.decoder(embedding)
+        return reconstructed
