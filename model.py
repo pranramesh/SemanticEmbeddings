71092a6c4b3e816e5f024edba5b945a105807b6e
Pranav Ramesh <pranavramesh4@gmail.com>
2024-01-24 15:02:10 -0800

    added temp beta-VAE implementation; need to add training loop

--- model.py
+++ model.py
@@ -1,11 +1,12 @@
-# import torch
-# import torch.nn as nn
-# import torch.optim as optim
+import torch
+import torch.nn as nn
+import torch.optim as optim
 import numpy as np
 import pandas as pd 
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
+from torch.autograd import Variable
 
 #### Alternative Idea ####
 # 1. Given some text from user, make a call to cohere language model api and ask it to make the statement more negative in connotation (geenrate 5 samples)
@@ -37,23 +38,34 @@ class Visualizations():
 # 4. The embedding is displayed
 # 5. User can choose how to toggle the embedding.
 # 6. The modified embedding is then passed through the decoder which gives back the modified text reconstructed purely from the latent space embedding
+# 7. Interpolate and show outputs between more negative/positive statement and original statement
 
-class Autoencoder(nn.Module):
+def reparametrize(mu, logvar):
+    std = logvar.div(2).exp()
+    eps = Variable(std.data.new(std.size()).normal_())
+    return mu + std*eps
+
+class BetaVAE(nn.Module):
     #use cosin smilarity for loss metric
-    def __init__(self, input_dim, embedding_dim):
-        super().__init__()
+    def __init__(self, input_dim, hidden_dim, latent_dim, beta):
+        super(BetaVAE, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Linear(in_features=input_dim, out_features=embedding_dim), 
+            nn.Linear(in_features=input_dim, out_features=hidden_dim), 
             nn.ReLU(), 
-            nn.Linear(in_features=embedding_dim, out_features=embedding_dim), 
+            nn.Linear(in_features=hidden_dim, out_features=(2*latent_dim)), 
         )
 
         self.decoder = nn.Sequential(
-            nn.Linear(in_features=embedding_dim, out_features=embedding_dim), 
+            nn.Linear(in_features=latent_dim, out_features=hidden_dim), 
             nn.ReLU(), 
-            nn.Linear(in_features=embedding_dim, out_features=input_dim), 
+            nn.Linear(in_features=hidden_dim, out_features=input_dim), 
         )
+
+        self.beta = beta 
+
     def forward(self, x):
-        embedding = self.encoder(x)
-        reconstructed = self.decoder(embedding)
+        h = self.encoder(x)
+        mu, logvar = torch.chunk(h, 2, dim=-1)
+        z = reparametrize(mu, logvar)
+        reconstructed = self.decoder(z)
         return reconstructed
